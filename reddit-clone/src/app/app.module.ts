ba7b301140c912e38ae4ccc74fcf5df889dcf42a
unknown <popovictodor01@gmail.com>
2022-05-09 23:29:47 +0200

    Pokusaj pocetne stranice

--- reddit-clone/src/app/app.module.ts
+++ reddit-clone/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from './components/subreddit-side-bar/subreddit-side-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SideBarComponent,
+    SubredditSideBarComponent,
+    NavBarComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
