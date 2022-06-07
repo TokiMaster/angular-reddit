9adada7188aaf824ecf0abf4e50a66f941c4b81a
unknown <popovictodor01@gmail.com>
2022-06-07 21:05:29 +0200

    Sitne prepravke

--- reddit-clone/src/app/guards/guard.ts
+++ reddit-clone/src/app/guards/guard.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { roleEnum } from '../model/roleEnum';
 import { AuthService } from '../service/auth.service';
-import { PostService } from '../service/post.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Guard implements CanActivate {
 
-  constructor(private auth:AuthService, private roleEnum: roleEnum){} 
+  constructor(private auth:AuthService){} 
 
   canActivate(
     next: ActivatedRouteSnapshot,
